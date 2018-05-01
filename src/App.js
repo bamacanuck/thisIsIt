404e2f611603b86e862c5b159bb8d4e64a9fae4f
bamacanuck <srandallscott@gmail.com>
2018-05-01 19:51:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,9 @@ class App extends Component {
 
     handleKeyPress = (event) => {
         if (event.key === 'Enter') {
-
+            let notesArr = this.state.notes;
+            notesArr.push(this.state.noteText);
+            this.setState({ noteText: '' });
         }
     };
 
